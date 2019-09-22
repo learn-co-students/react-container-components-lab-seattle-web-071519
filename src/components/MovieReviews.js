3c12406c45c6b62484004ab7b75b16acea713d0c
ayerest <irisiseve@gmail.com>
2019-09-22 12:34:24 -0700

    finished react container components lab

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,23 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = (props) => {
+    // console.log("props", props)
+    // debugger
+    let { byline, display_title, headline, publication_date, summary_short} = props.reviewData
+    return (
+        <div className="review-list">
+            <div className="review">
+                <ul>
+                    <li>{byline}</li>
+                    <li>{display_title}</li>
+                    <li>{headline}</li>
+                    <li>{publication_date}</li>
+                    <li>{summary_short}</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default MovieReviews
